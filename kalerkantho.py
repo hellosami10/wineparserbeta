372127e17ce7cb7f0f6ce40046d6eac5e120f812
Sami <hello.sami@yahoo.com>
2020-06-27 22:51:04 +0600

    Update kalerkantho.py

--- kalerkantho.py
+++ kalerkantho.py
@@ -1,35 +1,19 @@
-# from urllib.request import Request, urlopen   
-
-# from bs4 import BeautifulSoup
-# import requests
-
-# #specify url
-# url = 'https://www.kalerkantho.com/recent'
-# req = Request(url, headers={'User-Agent': 'XYZ/3.0'})
-# response = urlopen(req, timeout=20).read()
-
-
-# with open('ch.txt', "w", encoding="utf-8") as f:
-#     f.write()
-
-
-
-#Author: Sami, Adnan
-
+from urllib.request import Request, urlopen    
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://www.kalerkantho.com/recent')
-soup = BeautifulSoup(source.text, 'lxml')
+#specify url
+url = 'https://www.kalerkantho.com/recent'
+req = Request(url, headers={'User-Agent': 'XYZ/3.0'})
+response = urlopen(req, timeout=20).read()
 
-parent_tag = soup.find('div', class_='col-xs-12 headline').h2.text
+soup = BeautifulSoup(response, 'lxml')
 
-print(parent_tag)
+content = soup.find('div', class_='col-xs-12 headline').ul
+content = content.find_all('li')
 
-
-# for line in child_tag:
-# 	content = line.span.a.text
-# 	href = line.span.a['href']
-
-# 	# #print("<li onclick='copy(this.innerHTML)'><a target="_blank" href='https://www.prothomalo.com"+ href +"'>" + content + "</a></li>")
-# 	print("<li><a target='_blank' href='"+ href +"'>" + content + "</a></li>")
+for line in content:
+	content = line.a.text
+	href = line.a['href']
+	print("<li><a target='_blank' href='https://www.kalerkantho.com/"+ href +"'>" + content + "</a></li>")
+	print()
