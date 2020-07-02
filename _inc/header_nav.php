b5f61d758826822cf9eca455fa16e56553458fdb
Sami <hello.sami@yahoo.com>
2020-07-02 23:06:17 +0600

    Update header_nav.php

--- _inc/header_nav.php
+++ _inc/header_nav.php
@@ -1,8 +1,11 @@
+<?php 
+
+?>
 <div class="nav">
 	<ul>
-		<a href="crawl.php?str=activist"><li class="active">JAGONEWS24</li></a>
-		<a href="crawl.php?str=chipmunk"><li>PROTHOM ALO</li></a>
-		<a href="crawl.php?str=thunder"><li>BD PRATIDIN</li></a>
+		<a href="crawl.php?str=activist"><li class="<?php if($_GET['str'] == 'activist') echo 'active';?>">JAGONEWS24</li></a>
+		<a href="crawl.php?str=chipmunk" class="<?php if($_GET['str'] == 'chipmunk') echo 'active';?>"><li>PROTHOM ALO</li></a>
+		<a href="crawl.php?str=thunder" class="<?php if($_GET['str'] == 'thunder') echo 'active';?>"><li>BD PRATIDIN</li></a>
 		<a href="crawl.php?str=bdnews24"><li>BDNEWS24</li></a>
 		<a href="crawl.php?str=kalerkantho"><li>KALER KANTHO</li></a>
 		<a href="crawl.php?str=barta24"><li>BARTA24</li></a>
