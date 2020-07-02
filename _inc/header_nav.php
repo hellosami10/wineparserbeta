8929717a6e67e71573d451a04392cf32914f4f5f
Sami <hello.sami@yahoo.com>
2020-07-02 23:39:04 +0600

    Update header_nav.php

--- _inc/header_nav.php
+++ _inc/header_nav.php
@@ -1,14 +1,19 @@
 <?php 
+	function chk_active($str) {
 
+		if($_GET['str'] == $str)
+			echo 'active';
+
+	}
 ?>
 <div class="nav">
 	<ul>
-		<a href="crawl.php?str=activist"><li class="<?php if($_GET['str'] == 'activist') echo 'active';?>">JAGONEWS24</li></a>
-		<a href="crawl.php?str=chipmunk" class="<?php if($_GET['str'] == 'chipmunk') echo 'active';?>"><li>PROTHOM ALO</li></a>
-		<a href="crawl.php?str=thunder" class="<?php if($_GET['str'] == 'thunder') echo 'active';?>"><li>BD PRATIDIN</li></a>
-		<a href="crawl.php?str=bdnews24"><li>BDNEWS24</li></a>
-		<a href="crawl.php?str=kalerkantho"><li>KALER KANTHO</li></a>
-		<a href="crawl.php?str=barta24"><li>BARTA24</li></a>
-		<a href="crawl.php?str=banglatribune"><li>Bangla Tribune</li></a>
+		<a href="crawl.php?str=activist"><li class="<?php chk_active('activist');?>">JAGONEWS24</li></a>
+		<a href="crawl.php?str=chipmunk"><li class="<?php chk_active('chipmunk');?>">PROTHOM ALO</li></a>
+		<a href="crawl.php?str=thunder"><li class="<?php chk_active('thunder');?>">BD PRATIDIN</li></a>
+		<a href="crawl.php?str=bdnews24"><li class="<?php chk_active('bdnews24');?>">BDNEWS24</li></a>
+		<a href="crawl.php?str=kalerkantho"><li class="<?php chk_active('kalerkantho');?>">KALER KANTHO</li></a>
+		<a href="crawl.php?str=barta24"><li class="<?php chk_active('barta24');?>">BARTA24</li></a>
+		<a href="crawl.php?str=banglatribune"><li class="<?php chk_active('banglatribune');?>">Bangla Tribune</li></a>
 	</ul>
 </div>
